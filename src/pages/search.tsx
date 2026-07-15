ace5a3a41c643889f4edb978163b4dbfe03eef66
Pediatriasln <gustavoaugusto.medeiros@einstein.edu.br>
2026-07-15 17:22:49 -0300

    Add Search component with Layout and SearchBar

---
+++ src/pages/search.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+import SearchBar from "../components/SearchBar";
+
+export default function Search() {
+  return (
+    <Layout>
+      <h2 className="text-xl font-bold">Busca Inteligente</h2>
+      <SearchBar />
+    </Layout>
+  );
+}
